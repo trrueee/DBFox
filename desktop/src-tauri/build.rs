5de2b827cdd2e6c3c5aa98a0841d7ba2717fae31
jia.bin <2892602078@qq.com>
2026-06-14 20:02:47 +0800

    fix: rebuild tauri resources when icons change

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
+    for path in [
+        "tauri.conf.json",
+        "icons/32x32.png",
+        "icons/64x64.png",
+        "icons/128x128.png",
+        "icons/128x128@2x.png",
+        "icons/icon.png",
+        "icons/icon.icns",
+        "icons/icon.ico",
+    ] {
+        println!("cargo:rerun-if-changed={path}");
+    }
+
     tauri_build::build()
 }
